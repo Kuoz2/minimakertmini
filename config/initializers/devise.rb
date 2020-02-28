e3bf9c9588440358f281466b13ad611725a658fa
KuozDavid <david.palta.anes1989@gmail.com>
2020-02-28 16:57:48 -0300

    no esta funcionando esto

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -299,11 +299,12 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret =  '5eb705e5db0b7e7118916049a9490e2b0f8b2ceb4032a39c505726d876b11fb419514879743cad5ccc37a0d3c6eff74ce5bd31060c0f349e48d1731ad4853c9e'
     jwt.dispatch_requests = [
-        ['POST',%r{^/users/sign_in$}]
+        ['POST',%r{^/login$}]
     ]
     jwt.revocation_requests = [
-        ['DELETE', %r{^/users/sing_out$}]
+        ['DELETE', %r{^/logout$}]
     ]
+    jwt.request_formats = { user: [:json] }
     jwt.expiration_time = 1.days.to_i
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+             },
+             controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+             }
   resources :payments do
     resource :sales
   end
