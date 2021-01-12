861536c49813da6b128bb9842e0f8ad980333b09
KuozDavid <david.palta.anes1989@gmail.com>
2021-01-12 15:42:07 -0300

    cambiarminute

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -304,7 +304,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
         ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 2.minutes.to_i
+    jwt.expiration_time = 2.minutes
   end
 
 end
