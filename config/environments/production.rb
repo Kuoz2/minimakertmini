85b27a665023d8d5c0a831b310f008b4a97c8f26
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-10 17:33:18 -0400

    cambios configuracion heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host:'localhost:3000', protocol: 'http'}
+  config.action_mailer.default_url_options = {host:'https://marketmini.herokuapp.com/', protocol: 'https'}
   config.action_mailer.smtp_settings = {
    :addres => "smtp.gmail.com",
    :port => 587,
