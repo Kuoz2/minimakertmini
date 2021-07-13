970cb064169a84945e02959a89ebc8ccf4d0d8a0
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-13 19:38:22 -0400

    arreglando detalles

--- app/mailers/envio_ticket_mailer.rb
+++ app/mailers/envio_ticket_mailer.rb
@@ -2,7 +2,7 @@ class EnvioTicketMailer < ApplicationMailer
      #Envio de correo electronico cuando se agrega un nuevo producto.
   def new_envio_email
     @hola = 'david.palta.anes1989@gmail.com'
-    mail(to: @hola, subject: "Nuevo producto agregado", body: 'something')
+    mail(from: 'pruebadenviodecorreo@gmail.com', to: @hola, subject: "Nuevo producto agregado", body: 'something')
   end
 
 end
