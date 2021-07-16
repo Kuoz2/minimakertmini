63c3d9f14b502e998ba1dafcddfb183525ce1188
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-16 16:05:47 -0400

    probaod puertos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
    :addres => "smtp.gmail.com",
-   :port => 465,
+   :port => 587,
    :domain => "gmail.com",
    :user_name => Rails.application.credentials.dig(:google_smtp, :email),
    :password => Rails.application.credentials.dig(:google_smtp, :password),
