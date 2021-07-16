eff90e38ef2a144ff0de9c36943be88232cd272d
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-16 14:29:12 -0400

    envioemail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :herokuapp
   config.action_mailer.default_url_options = { :host => 'marketmini.herokuapp.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
