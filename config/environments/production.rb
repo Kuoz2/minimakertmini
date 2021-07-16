16cf8ed61dd9dd1197421779f4aa21e016c3e689
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-16 11:33:18 -0400

    no se debe guadar local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
   config.action_mailer.default_url_options = {host: 'marketmini.herokuapp.com', protocol: 'https'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
