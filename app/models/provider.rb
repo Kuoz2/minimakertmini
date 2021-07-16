4d693b7b154cd97d4034644a2e55572214da176d
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-16 11:26:16 -0400

    creo que ya esta

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,5 @@
 class Provider < ApplicationRecord
-  has_many :products, :inverse_of => :providers
-  has_many :stocks, :inverse_of => :providers
+  has_many :products#, :inverse_of => :providers
+  has_many :stocks#, :inverse_of => :providers
   accepts_nested_attributes_for :products, :stocks
 end
