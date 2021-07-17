8fd7bfb387babfc57cf29b18b838da61aca3ed99
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-17 12:32:16 -0400

    hola todo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
 default from: "pruebadenviodecorreo@gmail.com"
+#layout 'mailer'
 
 def new_envio_email
     @hola = 'david.palta.anes1989@gmail.com'
     @url = 'https://marketmini.herokuapp.com/products'
-    mail(from: 'pruebadenviodecorreo@gmail.com', to: @hola, subject: "Nuevo producto agregado",template_path: 'notifications',
-        template_name: 'another')
+    mail from: 'pruebadenviodecorreo@gmail.com', to: @hola, subject: "Nuevo producto agregado" 
 end
 
 end
