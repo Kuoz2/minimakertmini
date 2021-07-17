e43961d82aba51f59de5dfd5b83ffa3bcfb1fc6e
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-17 12:41:46 -0400

    dsadn

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,7 +5,7 @@ default from: "pruebadenviodecorreo@gmail.com"
 def new_envio_email
     @hola = 'david.palta.anes1989@gmail.com'
     @url = 'https://marketmini.herokuapp.com/products'
-    mail from: 'pruebadenviodecorreo@gmail.com', to: @hola, subject: "Nuevo producto agregado" 
+    mail(from: 'pruebadenviodecorreo@gmail.com', to: @hola, subject: "Nuevo producto agregado" )
 end
 
 end
