ec5f77ae62ac7cefa6b2504aa514c1035823e440
KuozDavid <david.palta.anes1989@gmail.com>
2021-07-17 12:28:55 -0400

    hola todo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,9 +5,7 @@ def new_envio_email
     @hola = 'david.palta.anes1989@gmail.com'
     @url = 'https://marketmini.herokuapp.com/products'
     mail(from: 'pruebadenviodecorreo@gmail.com', to: @hola, subject: "Nuevo producto agregado",template_path: 'notifications',
-        template_name: 'another') do | formart |
-            format.text
-        end
+        template_name: 'another')
 end
 
 end
