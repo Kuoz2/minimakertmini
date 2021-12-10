6e2a2298a12517c09c4d5d7704e374f92bcee5f1
Kuoz2 <david.palta.anes1989@gmail.com>
2021-12-10 14:34:20 -0300

    probando cambios

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :products#,:source => :category
   accepts_nested_attributes_for :products
- 
   attr_accessor :verificacion_jtil
+  self.changes
+  
 end
