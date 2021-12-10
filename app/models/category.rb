ed54c1b4179ece9ae141200b1413c6f8b825ac10
Kuoz2 <david.palta.anes1989@gmail.com>
2021-12-10 14:54:19 -0300

    recargando

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   has_many :products#,:source => :category
   accepts_nested_attributes_for :products
   attr_accessor :verificacion_jtil
-  self.changes
-  
+  after_save :recargar
+  def recargar
+    category.reloaded
+  end
 end
