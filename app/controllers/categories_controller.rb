b9e13e5f359b71bb4fa09249cf5fc4d77316bc70
Kuoz2 <david.palta.anes1989@gmail.com>
2021-12-11 17:34:46 -0300

    cambiandocosas

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -39,7 +39,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     if @category.save
 
-      render json: {guardado:'correctamente'}, status: :created, location: @category
+      render json: @category, status: :created, location: @category
     else
       render json: @category.errors, status: :not_found
     end
