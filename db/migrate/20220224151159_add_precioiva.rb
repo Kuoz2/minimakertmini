ed2375c51d8b21f0703d7beca97de68c8e190038
Kuoz2 <david.palta.anes1989@gmail.com>
2022-02-24 12:31:33 -0300

    subiendo cambio de products

--- db/migrate/20220224151159_add_precioiva.rb
+++ db/migrate/20220224151159_add_precioiva.rb
@@ -1,6 +1,6 @@
 class AddPrecioiva < ActiveRecord::Migration[6.0]
   def change
-    add_column :product, :preciva, :bigint, :default => 0
+    add_column :products, :preciva, :bigint, :default => 0
     #Ex:- :default =>''
   end
 end
